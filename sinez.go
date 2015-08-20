2592dfc8e9fc72170b1b5d44bf56d415bdfbd5c0
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2015-08-19 21:19:30 -0700

    mehh

--- sinez.go
+++ sinez.go
@@ -19,7 +19,7 @@ func newSine(sinezChan chan *stereoSine, freq float64) {
 }
 
 func newStereoSine(freq float64) *stereoSine {
-	s := &stereoSine{sine: &oscillator{vol: 1, freq: freq, phase: 0, phaseIncr: freq * freqRad}}
+	s := &stereoSine{sine: &oscillator{vol: 0.6, freq: freq, phase: 0, phaseIncr: freq * freqRad}}
 	var err error
 	s.Stream, err = portaudio.OpenDefaultStream(0, 2, sampleRate, 0, s.processAudio)
 	chk(err)
@@ -59,38 +59,10 @@ func (g *stereoSine) set(property string, val float64) {
 }
 
 func (g *stereoSine) processAudio(out [][]float32) {
-	// melodyting
-	// scale := 1.58
-
-	// SAWTOOTH
-	// sawIncr := 2 * g.freq / sampleRate
-	// sawVal := float64(-1)
-
-	// triable
-	//twoDivPi := 2.0 / math.Pi
-
-	// square
-	// midPoint := twoPi *
 
 	for i := range out[0] {
-		// value := float64(0)
-		// for p := float64(1); p <= 4; p++ {
-		// 	value += float64(math.Sin(g.phase*p)) / p
-		// }
-		// g.phase += g.phaseIncr
-		// if g.phase >= twoPi {
-		// 	g.phase -= twoPi
-		// }
-		// out[0][i] = g.vol * float32(value/scale*g.time/bpm)
-		// out[1][i] = g.vol * float32(value/scale*g.time/bpm)
 
-		//out[0][i] = g.vol * float32(math.Sin(g.phase*(g.time/bpm)))
-		//out[1][i] = g.vol * float32(math.Sin(g.phase*(g.time/bpm)))
-		//out[0][i] = float32(g.sine.amp * math.Sin(g.sine.phase*bpm))
-		//out[1][i] = float32(g.sine.amp * math.Sin(g.sine.phase*bpm))
-		//g.sine.phase = g.sine.phase + g.sine.phaseIncr
-
-		// simple sine
+		// sine
 		out[0][i] = float32(g.sine.vol * math.Sin(g.sine.phase))
 		out[1][i] = float32(g.sine.vol * math.Sin(g.sine.phase))
 		g.sine.phase += g.sine.phaseIncr
@@ -105,24 +77,5 @@ func (g *stereoSine) processAudio(out [][]float32) {
 		//if sawVal >= 1 {
 		//	sawVal -= 2
 		//}
-
-		// triangle wave
-		// triVal := g.phase * twoDivPi
-		// if triVal < 0 {
-		// 	triVal = 1.0 + triVal
-		// } else {
-		// 	triVal = 1.0 - triVal
-		// }
-		// out[0][i] = g.vol * float32(triVal)
-		// out[1][i] = g.vol * float32(triVal)
-
-		// square
-
-		// g.phase += g.phaseIncr
-		// if g.phase >= twoPi {
-		// 	g.phase -= twoPi
-		// }
-
-		g.time++
 	}
 }
